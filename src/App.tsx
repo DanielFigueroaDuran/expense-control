0d64e05086cd79644ae3f73643eed66c45fd37aa
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-07-05 19:32:56 +0200

    expense_control

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Control de Gasto
+        </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
