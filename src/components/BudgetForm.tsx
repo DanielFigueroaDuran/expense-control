dae71ba196a3878bd3580ba42f69ef83f3a5fc61
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-07-05 21:52:29 +0200

    Validating the Budget

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -4,8 +4,13 @@ const BudgetForm = () => {
       const [budget, setBudget] = useState(0);
 
       const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setBudget(e.target.valueAsNumber);
+            const value = e.target.valueAsNumber;
+            // If the user deleted the entire number, we force a 0 in the state.
+            setBudget(isNaN(value) ? 0 : value);
+
       };
+
+      const isInvalidBudget = budget <= 0;
 
       return (
             <form className="space-y-5">
@@ -19,14 +24,19 @@ const BudgetForm = () => {
                               className="w-full bg-white border border-gray-200 p-2"
                               placeholder="Define tu Presupuesto"
                               name="budget"
-                              value={budget}
+                              value={budget === 0 ? '' : budget}
                               onChange={handleChange}
                         />
                   </div>
                   <input
                         type="submit"
                         value='Definir Presupuesto'
-                        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+                        className={`w-full p-3 font-bold text-white rounded transition-colors duration-200 
+                              ${isInvalidBudget
+                                    ? "bg-slate-700 text-slate-400 cursor-not-allowed opacity-40"
+                                    : "bg-blue-600 hover:bg-blue-700 cursor-pointer"
+                              }`}
+                        disabled={isInvalidBudget}
                   />
             </form>
       )
