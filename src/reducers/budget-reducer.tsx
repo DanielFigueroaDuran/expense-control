3f86df76d753eb6607da5ccbd0325c2f7ef1a5d2
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-08-18 19:20:35 +0200

    Showing the modal when adding a new expense

--- src/reducers/budget-reducer.tsx
+++ src/reducers/budget-reducer.tsx
@@ -1,13 +1,15 @@
-export type BudgetActions = {
-      type: 'add-budget', payload: { budget: number }
-};
+export type BudgetActions =
+      { type: 'add-budget', payload: { budget: number } } |
+      { type: 'show-modal' }
 
 export type BudgetState = {
-      budget: number
+      budget: number,
+      modal: boolean
 };
 
 export const initialState: BudgetState = {
-      budget: 0
+      budget: 0,
+      modal: false
 };
 
 export const budgetReducer = (
@@ -19,6 +21,12 @@ export const budgetReducer = (
                   ...state,
                   budget: action.payload.budget
             };
+      };
+
+      if (action.type === 'show-modal') {
+            return {
+                  ...state, modal: true
+            }
       };
       return state
 };
