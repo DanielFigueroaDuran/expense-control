0e25c66f79bf2d19ba4762c189ac5a315edb97b4
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-08-20 21:55:20 +0200

    Defining the type for expenses

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,19 @@
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
 import { categories } from "../data/categories"
+import { useState } from 'react';
+import type { DraftExpense } from '../types';
+
 
 const ExpenseForm = () => {
+      const [expense, setExpense] = useState<DraftExpense>({
+            amount: 0,
+            expenseName: '',
+            category: '',
+            date: new Date()
+      });
+
       return (
             <form className="space-y-5">
                   <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -20,6 +33,7 @@ const ExpenseForm = () => {
                               placeholder="Añade el Nombre del Gasto"
                               className="bg-slate-100 p-2"
                               name="expenseName"
+                              value={expense.expenseName}
                         />
                   </div>
                   <div className="flex flex-col gap-2">
@@ -36,6 +50,7 @@ const ExpenseForm = () => {
                               placeholder="Añade la Cantidad del Gasto"
                               className="bg-slate-100 p-2"
                               name="amount"
+                              value={expense.amount}
                         />
                   </div>
                   <div className="flex flex-col gap-2">
@@ -50,6 +65,7 @@ const ExpenseForm = () => {
                               id="category"
                               className="bg-slate-100 p-2"
                               name="category"
+                              value={expense.category}
                         >
                               <option value="">-- Seleccione --</option>
                               {categories.map(category => (
@@ -62,12 +78,25 @@ const ExpenseForm = () => {
                               ))}
                         </select>
                   </div>
+                  <div className="flex flex-col gap-2">
+                        <label
+                              htmlFor="date"
+                              className="text-xl"
+                        >
+                              Fecha Gasto
+
+                        </label>
+
+                        <DatePicker
+                              className="bg-slate-100 p-2 border-0"
+                              value={expense.date}
+                        />
+                  </div>
 
                   <input
                         type="submit"
                         className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
                         value={'Registrar Gasto'}
-                  //input
                   />
             </form>
       )
